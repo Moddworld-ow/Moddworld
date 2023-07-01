783ad3a7b2fa8d90815473aefe48720b8ad00c4e
Paul <pawlogates123@gmail.com>
2023-07-01 10:44:58 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,33 @@
+let color = 'red';
+console.log(color);
+
+let thing = {
+
+name1: 'hey',
+age: 6
+
+};
+
+console.log(thing);
+
+console.log(thing.name1);
+
+
+
+
+function sayname(myname) {
+
+    console.log('hello ' + myname);
+
+}
+
+sayname('Pawel');
+
+function weird(number) {
+
+return number * 7 + 2 + number*0.3 - 4;
+}
+
+let newnumber = [weird(852)];
+
+console.log(newnumber)
